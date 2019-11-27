fa214e42a44ce1c897fca4eb684e8ad813216749
wisara88 <srandall845@gmail.com>
2019-11-26 20:03:42 -0800

    storing keys

--- main.js
+++ main.js
@@ -6,27 +6,30 @@ document.addEventListener("keydown", function(event) {
     var keyStrokes = [];
     for (var i = 0; i < allTags.length; i++) {
       var newAtt = allTags[i];
-      // var tagText = newAtt.textContent;
+      var tagText = newAtt.textContent;
       var shortCut = document.createElement('div');
-      // var span1 = document.createElement('span');
       var span2 = document.createElement('span');
 
-      // span1.textContent = "A";
-      span2.textContent = i;
-      // shortCut.appendChild(span1);
+      span2.textContent = tagText.slice(0,1) + i;
+      // span2.onclick(newAtt.click());
+      span2.onclick = () => newAtt.click();
+      // span2.onclick = () => console.log("whoooo");
+
+      // function clickIt(newAtt) {
+      //   newAtt.click();
+      // }
+      // span2.onclick = clickIt;
       shortCut.appendChild(span2);
-      // newAtt.appendChild(shortCut);
       newAtt.insertAdjacentElement('afterend', shortCut);
       displayArray.push(span2.textContent);
     }
     document.addEventListener("keydown", function(e) {
-      console.log("e" + e.which);
-      console.log(displayArray);
-      keyStrokes.push(e);
-      var list = displayArray.filter(num => num.startsWith(e) );
-      console.log(list);
+      var letter = e.keyCode;
+      // console.log(displayArray);
+      keyStrokes.push(String.fromCharCode(letter));
+      console.log(keyStrokes);
+      // var list = displayArray.filter(num => num.startsWith(e) );
     })
-    // console.log(displayArray);
   }
 })
 
