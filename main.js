4e16b7b331e56293626a9eff9fa79442116ba39a
wisara88 <srandall845@gmail.com>
2019-12-03 18:46:39 -0800

    filtering to one div

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ document.addEventListener("keydown", function(event) {
   if (event.which == 70) {
     var displayArray = [];
     var keyStrokes = [];
+    let keyCount = 0;
+    let list = [];
+
     for (var i = 0; i < allTags.length; i++) {
       const newAtt = allTags[i];
       var tagText = newAtt.textContent;
@@ -23,11 +26,23 @@ document.addEventListener("keydown", function(event) {
       displayArray.push(span2.textContent);
     }
     document.addEventListener("keydown", function(e) {
-      var letter = e.keyCode;
+      let letter = e.keyCode;
       // console.log(displayArray);
       keyStrokes.push(String.fromCharCode(letter));
       console.log(keyStrokes);
-      // var list = displayArray.filter(num => num.startsWith(e) );
+      if (keyCount === 0) {
+        list = displayArray.filter(key => key.startsWith(String.fromCharCode(letter)) );
+      } else {
+        list = list.filter(key => key.includes(String.fromCharCode(letter)) );
+      }
+
+      // let list = displayArray.filter(function(key) {
+      //   return key.slice(keyCount, (keyCount + 1));
+      //   key.
+      // });
+      keyCount += 1;
+      console.log("list", list);
+      console.log("keyCount", keyCount);
     })
   }
 })
