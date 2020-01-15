34495c53506974729d29367a1a5863dfc21f8e08
wisara88 <srandall845@gmail.com>
2020-01-14 18:53:32 -0800

    clickable autogenerated tags working

--- main.js
+++ main.js
@@ -13,37 +13,38 @@ document.addEventListener("keydown", function(event) {
       var shortCut = document.createElement('div');
       var span2 = document.createElement('span');
 
-      span2.textContent = tagText.slice(0,1) + i;
-      // span2.onclick(newAtt.click());
+      span2.textContent = tagText.slice(0,1) + (i +10000);
+      span2.setAttribute("id", span2.textContent)
       span2.onclick = () => {
         newAtt.click();
-        // console.log(newAtt);
         }
 
       shortCut.appendChild(span2);
       newAtt.insertAdjacentElement('afterend', shortCut);
-      // console.log(tagText);
+
       displayArray.push(span2.textContent);
     }
     document.addEventListener("keydown", function(e) {
       let letter = e.keyCode;
-      // console.log(displayArray);
+
       keyStrokes.push(String.fromCharCode(letter));
       console.log(keyStrokes);
       if (keyCount === 0) {
         list = displayArray.filter(key => key.startsWith(String.fromCharCode(letter)) );
       } else {
-        list = list.filter(key => key.includes(String.fromCharCode(letter)) );
+        // list = list.filter(key => key.includes(String.fromCharCode(letter)) );
+        list = displayArray.filter(key => key.startsWith(keyStrokes.join("")) );
+
       }
 
       if (list.length <= 1) {
-        // const clickMe = document.documentElement.textContent(list[0]);
-        const clickMe = document.documentElement.textContent.indexOf(list[0]);
-        console.log(clickMe);
-        // const clickMe = document.documentElement.innerText()
 
-
-        clickMe.click();
+        const clickMe = document.documentElement.innerHTML
+        console.log("clickMe", clickMe);
+        // clickMe.click();
+        const selection = document.querySelector('#'+list[0]);
+        console.log("selection", selection);
+        selection.click();
       }
       // let list = displayArray.filter(function(key) {
       //   return key.slice(keyCount, (keyCount + 1));
@@ -58,3 +59,4 @@ document.addEventListener("keydown", function(event) {
 
 
 
+// windows.location.assign("http://url")
